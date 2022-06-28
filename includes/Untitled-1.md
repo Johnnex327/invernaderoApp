4c6c266826330443af86f8414d300927bbd45a29
Jhon <jhon.naib327@gmail.com>
2022-06-28 00:38:29 -0500

    segundo commit

---
+++ includes/Untitled-1.md
@@ -0,0 +1,5 @@
+mysql://
+b9ca552a2102d8
+1e515fa5
+us-cdbr-east-05.cleardb.net
+heroku_7d32585ee9f63f6
--- includes/setData.php
+++ includes/setData.php
@@ -1,34 +1,29 @@
-<?php
+<?php 
+
+    include 'connect.php';
+    $connect = mysqli_connect($hostname, $username, $password, $database);
+
+    date_default_timezone_set("America/Guayaquil");
+    $fecha = date("Y-m-d") ?? '2022-06-27 00:00:00';
+
+    $riegos = $_POST['riegos'] ??  1;
+    $h_suelo = $_POST['h_suelo'] ??  30;
+    $temperatura = $_POST['temperatura'] ??  32;
+    $h_relativa = $_POST['h_relativa'] ??  65;
+    $id_micro = $_POST['id_micro'] ??  4;
 
 
-$connect = mysqli_connect(
-    $_ENV['DB_HOST'],
-    $_ENV['DB_USER'],
-    $_ENV['DB_PASS'] ?? '',
-    $_ENV['DB_BD']
-);
+    $query = "SELECT * FROM control_riego WHERE fecha = '".$fecha."' ";
+    $resultado = mysqli_query($connect, $query);
+    
+    if($resultado->num_rows === 1){ //Actualiza un registro existente
 
-date_default_timezone_set("America/Guayaquil");
-$fecha = date("Y-m-d") ?? '2022-06-27 00:00:00';
+        //Programar el promedio se humedad / 2
 
-$riegos = $_POST['riegos'] ??  2;
-$h_suelo = $_POST['h_suelo'] ??  45;
-$temperatura = $_POST['temperatura'] ??  34;
-$h_relativa = $_POST['h_relativa'] ??  73;
-$id_micro = $_POST['id_micro'] ??  3;
+        $result=$connect->query("UPDATE control_riego SET riegos='".$riegos."', h_suelo='".$h_suelo."', temperatura='".$temperatura."',  h_relativa='".$h_relativa."', id_micro='".$id_micro."' WHERE fecha='".$fecha."'");
+    }else{//Crea un registro si no existe la fecha en la BD
 
+        $result=$connect->query("INSERT INTO control_riego (fecha, riegos, h_suelo, temperatura, h_relativa, id_micro) VALUES ('".$fecha."', '".$riegos."', '".$h_suelo."', '".$temperatura."', '".$h_relativa."', '".$id_micro."');");
+    }
 
-$query = "SELECT * FROM control_riego WHERE fecha = '" . $fecha . "' ";
-$resultado = mysqli_query($connect, $query);
-
-if ($resultado->num_rows === 1) { //Actualiza un registro existente
-
-    //Programar el promedio se humedad / 2
-
-    $result = $connect->query("UPDATE control_riego SET riegos='" . $riegos . "', h_suelo='" . $h_suelo . "', temperatura='" . $temperatura . "',  h_relativa='" . $h_relativa . "', id_micro='" . $id_micro . "' WHERE fecha='" . $fecha . "'");
-} else { //Crea un registro si no existe la fecha en la BD
-
-    $result = $connect->query("INSERT INTO control_riego (fecha, riegos, h_suelo, temperatura, h_relativa, id_micro) VALUES ('" . $fecha . "', '" . $riegos . "', '" . $h_suelo . "', '" . $temperatura . "', '" . $h_relativa . "', '" . $id_micro . "');");
-}
-
-mysqli_close($connect);
+    mysqli_close($connect);
