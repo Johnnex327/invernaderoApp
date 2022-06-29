3047f29db091ca9920937b4acf2d9fb5015fd3b9
Jhon <jhon.naib327@gmail.com>
2022-06-28 22:25:19 -0500

    treintatres commit

--- includes/connect.php
+++ includes/connect.php
@@ -1,10 +1,10 @@
 <?php 
-/* $hostname ="us-cdbr-east-05.cleardb.net";
+$hostname ="us-cdbr-east-05.cleardb.net";
 $database ="heroku_7d32585ee9f63f6";
 $username ="b9ca552a2102d8";
-$password ="1e515fa5"; */
+$password ="1e515fa5";
 
-$hostname ="localhost";
+/* $hostname ="localhost";
 $database ="invernadero";
 $username ="root";
-$password =""; 
+$password ="";  */
