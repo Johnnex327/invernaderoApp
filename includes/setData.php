414a145f1e1191349b78c3cea70e531ba24fcc45
Jhon <jhon.naib327@gmail.com>
2022-08-16 16:22:55 -0500

    actualizacion

--- includes/setData.php
+++ includes/setData.php
@@ -11,6 +11,8 @@
     $temperatura = $_POST['temperatura'] ??  32;
     $h_relativa = $_POST['h_relativa'] ??  65;
     $id_micro = $_POST['id_micro'] ?? 10;
+    $texto = "hola";
+
 
     $query = "SELECT * FROM control_riego WHERE fecha = '".$fecha."' AND id_micro= '$id_micro' ";
     $resultado = mysqli_query($connect, $query);
@@ -21,12 +23,12 @@
 
         //Programar el promedio se humedad / 2
 
-        $result=$connect->query("UPDATE control_riego SET riegos='".$riegos."', h_suelo='".$h_suelo."', temperatura='".$temperatura."',  h_relativa='".$h_relativa."', id_micro='".$id_micro."', WHERE fecha='".$fecha."' AND id_micro='".$id_micro."' ");
+        $result=$connect->query("UPDATE control_riego SET riegos='".$riegos."', h_suelo='".$h_suelo."', temperatura='".$temperatura."',  h_relativa='".$h_relativa."', id_micro='".$id_micro."', texto='".$texto."' WHERE fecha='".$fecha."' AND id_micro='".$id_micro."' ");
     }
     
     if($resultado->num_rows === 0){//Crea un registro si no existe la fecha en la BD
 
-        $result=$connect->query("INSERT INTO control_riego (fecha, riegos, h_suelo, temperatura, h_relativa, id_micro) VALUES ('".$fecha."', '".$riegos."', '".$h_suelo."', '".$temperatura."', '".$h_relativa."', '".$id_micro."' );");
+        $result=$connect->query("INSERT INTO control_riego (fecha, riegos, h_suelo, temperatura, h_relativa, id_micro, texto) VALUES ('".$fecha."', '".$riegos."', '".$h_suelo."', '".$temperatura."', '".$h_relativa."', '".$id_micro."', '".$texto."' );");
     }
 
     mysqli_close($connect);
